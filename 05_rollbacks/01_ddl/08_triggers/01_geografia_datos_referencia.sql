4764fdc6d2e03f1f29a544ac2cf961de9c6d52e1
Jhon Camilo Suaza <jhon04suazasanchez@gmail.com>
2026-04-15 21:32:07 -0500

    feat(HU-02): integrar dominio funcional 01_geografia_datos_referencia con tablas, indices, funciones, triggers y rollbacks integrados

---
+++ 05_rollbacks/01_ddl/08_triggers/01_geografia_datos_referencia.sql
@@ -0,0 +1,8 @@
+DROP TRIGGER IF EXISTS trg_time_zone_upd ON time_zone;
+DROP TRIGGER IF EXISTS trg_continent_upd ON continent;
+DROP TRIGGER IF EXISTS trg_country_upd ON country;
+DROP TRIGGER IF EXISTS trg_state_province_upd ON state_province;
+DROP TRIGGER IF EXISTS trg_city_upd ON city;
+DROP TRIGGER IF EXISTS trg_district_upd ON district;
+DROP TRIGGER IF EXISTS trg_address_upd ON address;
+DROP TRIGGER IF EXISTS trg_currency_upd ON currency;
