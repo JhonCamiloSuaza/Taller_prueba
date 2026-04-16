9ed9c06d7c8f1cf9dfda0ff02267f3272958b47c
Jhon Camilo Suaza <jhon04suazasanchez@gmail.com>
2026-04-16 18:23:19 -0500

    feat: HU-07 integracion liquibase dominio aeropuerto

---
+++ 05_rollbacks/01_ddl/08_triggers/06_aeropuerto.sql
@@ -0,0 +1,5 @@
+DROP TRIGGER IF EXISTS trg_airport_regulation_upd ON airport_regulation;
+DROP TRIGGER IF EXISTS trg_runway_upd ON runway;
+DROP TRIGGER IF EXISTS trg_boarding_gate_upd ON boarding_gate;
+DROP TRIGGER IF EXISTS trg_terminal_upd ON terminal;
+DROP TRIGGER IF EXISTS trg_airport_upd ON airport;
